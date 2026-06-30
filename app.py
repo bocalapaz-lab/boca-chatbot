0c4fefd06cf5bcc1d1388c55ec5b2c4c8b1d0f1d
Cesar <bocalapaz@gmail.com>
2026-06-29 23:19:40 -0600

    Desactivar modo debug para produccion

--- app.py
+++ app.py
@@ -534,4 +534,4 @@ def enviar_confirmacion_llamada(number):
     enviar_payload(data)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=False)
